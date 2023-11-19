f4573570cdacdcfea1a018172f4b17cad1410712
tasnim5sanzida@gmail.com <tasnim5sanzida@gmail.com>
2023-11-19 22:15:47 +0600

    navigation

--- assets/front/js/front.js
+++ assets/front/js/front.js
@@ -34,8 +34,7 @@ jQuery(function($){
             }
         });
         $('#btn').on('click', function() {
-            
-            // Retrieve the text or value of active children
+
             let activeChildren = $(".cbs-hour[data='true']");
             if (activeChildren.length > 0) {
                 activeChildren.each(function () {
@@ -49,6 +48,78 @@ jQuery(function($){
             let date = $('#calendar').val();
             console.log(date);
         });
+
+        if ($(".cbs-form-area").is(":visible")) {
+            $(".cbs-selected-tab").addClass("blue");
+        }
+        $(".btn-area").on("click", function () {
+            // Check which area is currently visible
+            if ($(".cbs-form-area").is(":visible")) {
+                // Hide slot area and show review area  
+                $(".cbs-form-area").hide();
+                $(".confirm-area").hide();
+                $(".review-area").show();
+                $(".cbs-selected-tab").removeClass("blue");
+                $(".cbs-review-tab").addClass("blue");
+                $(".prev-area button").prop('disabled', false);
+            } else if ($(".review-area").is(":visible")) {
+                // Hide review area and show payment area
+                $(".review-area").hide();
+                $(".confirm-area").hide();
+                $(".payment-area").show();
+                $(".cbs-review-tab").removeClass("blue");
+                $(".cbs-payment-tab").addClass("blue");
+                $(".prev-area button").prop('disabled', false);
+            } else if ($(".payment-area").is(":visible")) {
+                // Hide review area and show payment area
+                $(".review-area").hide();
+                $(".payment-area").hide();
+                $(".confirm-area").show();
+                $(".prev-area button").prop('disabled', false);
+                $(".cbs-payment-tab").removeClass("blue");
+                $(".cbs-confirm-tab").addClass("blue");
+            }
+        });
+
+        $(".prev-area").on("click", function () {
+            // Check which area is currently visible
+            if ($(".review-area").is(":visible")) {
+                // Hide slot area and show review area
+                $(".cbs-form-area").show();
+                $(".payment-area").hide();
+                $(".review-area").hide();
+                $(".confirm-area").hide();
+                $(".cbs-selected-tab").addClass("blue");
+                $(".cbs-review-tab").removeClass("blue");
+
+            } else if ($(".payment-area").is(":visible")) {
+                // Hide review area and show payment area
+                $(".review-area").show();
+                $(".cbs-form-area").hide();
+                $(".payment-area").hide();
+                $(".confirm-area").hide();
+                $(".cbs-review-tab").addClass("blue");
+                $(".cbs-payment-tab").removeClass("blue");
+
+            } else if ($(".confirm-area").is(":visible")) {
+                // Hide review area and show payment area
+                $(".payment-area").show();
+                $(".review-area").hide();
+                $(".cbs-form-area").hide();
+                $(".confirm-area").hide();
+                $(".cbs-payment-tab").addClass("blue");
+                $(".cbs-confirm-tab").removeClass("blue");
+
+            }  else {
+                $(".prev-area button").prop('disabled', true);
+            }
+            
+        });
+
+
+
+
+
     });
     
 
--- inc/Shortcode.php
+++ inc/Shortcode.php
@@ -15,10 +15,10 @@ class Shortcode{
         <div class="cbs-container">
 
             <div class="cbs-tab-section">
-                <div class="cbs-tab">Select Slots</div> >
-                <div class="cbs-tab">Review</div> >
-                <div class="cbs-tab">Payment</div> >
-                <div class="cbs-tab">Confirmation</div>
+                <div class="cbs-tab cbs-selected-tab">Select Slots</div> >
+                <div class="cbs-tab cbs-review-tab">Review</div> >
+                <div class="cbs-tab cbs-payment-tab">Payment</div> >
+                <div class="cbs-tab cbs-confirm-tab">Confirmation</div>
             </div>
             
             <div class="cbs-form-area">
@@ -28,7 +28,7 @@ class Shortcode{
                     </div>
                 </div>
                 <div class="hour-picker">
-                    <div class="left-ara">
+                    <div class="left-area">
                         <div class="cbs-hour">Morning</div>
                         <div class="cbs-hour">
                             <p>07h-08h</p>
@@ -163,9 +163,27 @@ class Shortcode{
                 </div>
             </div>
 
-            <div class="btn-area">
-                <button type="button" id="btn">Next</button>
+            <div class="review-area content">
+                <h3>This is review area</h3>
             </div>
+
+            <div class="payment-area content">
+                <h3>This is Payment area</h3>
+            </div>
+
+            <div class="confirm-area content">
+                <h3>This is Confirm area</h3>
+            </div>
+
+            <div class="all-button">
+                <div class="prev-area">
+                    <button type="button" id="btn">Prev</button>
+                </div>
+                <div class="btn-area">
+                    <button type="button" id="btn">Next</button>
+                </div>
+            </div>
+
         </div>
        <?php 
     }
