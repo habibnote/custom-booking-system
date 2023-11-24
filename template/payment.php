4b06bec5bcc691fe368bfec7f9457b6a750fab96
Habibur Rahman <habibblog97@gmail.com>
2023-11-24 11:52:14 +0600

    filup all programatically

--- template/payment.php
+++ template/payment.php
@@ -1,34 +1,34 @@
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-<div id="payment" class="woocommerce-checkout-payment">
-	<?php if ( WC()->cart->needs_payment() ) : ?>
-		<ul class="wc_payment_methods payment_methods methods">
-			<?php
-			if ( ! empty( $available_gateways ) ) {
-				foreach ( $available_gateways as $gateway ) {
-                    ?>
-                    <li class="wc_payment_method payment_method_<?php echo esc_attr( $gateway->id ); ?>">
-                        <input id="payment_method_<?php echo esc_attr( $gateway->id ); ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+    <div id="payment" class="woocommerce-checkout-payment">
+        <?php if ( WC()->cart->needs_payment() ) : ?>
+            <ul class="wc_payment_methods payment_methods methods">
+                <?php
+                if ( ! empty( $available_gateways ) ) {
+                    foreach ( $available_gateways as $gateway ) {
+                        ?>
+                        <li class="wc_payment_method payment_method_<?php echo esc_attr( $gateway->id ); ?>">
+                            <input id="payment_method_<?php echo esc_attr( $gateway->id ); ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
 
-                        <label for="payment_method_<?php echo esc_attr( $gateway->id ); ?>">
-                            <?php echo $gateway->get_title(); /* phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped */ ?> <?php echo $gateway->get_icon(); /* phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped */ ?>
-                        </label>
-                        <?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
-                            <div class="payment_box payment_method_<?php echo esc_attr( $gateway->id ); ?>" <?php if ( ! $gateway->chosen ) : /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>style="display:none;"<?php endif; /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>>
-                                <?php $gateway->payment_fields(); ?>
-                            </div>
-                        <?php endif; ?>
-                    </li>
-
-
-                    <?php
+                            <label for="payment_method_<?php echo esc_attr( $gateway->id ); ?>">
+                                <?php echo $gateway->get_title(); /* phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped */ ?> <?php echo $gateway->get_icon(); /* phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped */ ?>
+                            </label>
+                            <?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
+                                <div class="payment_box payment_method_<?php echo esc_attr( $gateway->id ); ?>" <?php if ( ! $gateway->chosen ) : /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>style="display:none;"<?php endif; /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace */ ?>>
+                                    <?php $gateway->payment_fields(); ?>
+                                </div>
+                            <?php endif; ?>
+                        </li>
 
 
-				}
-			} 
-			?>
-		</ul>
-	<?php endif; ?>
-</div>
+                        <?php
+
+
+                    }
+                } 
+                ?>
+            </ul>
+        <?php endif; ?>
+    </div>
 
 </form>
