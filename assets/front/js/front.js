6752e1c7af87c0ca1e017da3a6d8f426a3d0bb7e
Habibur Rahman <habibblog97@gmail.com>
2023-11-24 09:17:42 +0600

    tab menu conditionally rendaring done

--- assets/front/js/front.js
+++ assets/front/js/front.js
@@ -52,87 +52,87 @@ jQuery(function($){
         //     }
         // });
 
-        $('#btn').on('click', function() {
+        // $('#btn').on('click', function() {
 
-            let activeChildren = $(".cbs-hour[data='true']");
-            if (activeChildren.length > 0) {
-                activeChildren.each(function () {
-                    let value = $(this).text();
-                    console.log(value);
-                });
-            }else{
-                console.log("Please select a hour");
-            }
+        //     let activeChildren = $(".cbs-hour[data='true']");
+        //     if (activeChildren.length > 0) {
+        //         activeChildren.each(function () {
+        //             let value = $(this).text();
+        //             console.log(value);
+        //         });
+        //     }else{
+        //         console.log("Please select a hour");
+        //     }
 
-            let date = $('#calendar').val();
-            console.log(date);
-        });
+        //     let date = $('#calendar').val();
+        //     console.log(date);
+        // });
 
-        if ($(".cbs-form-area").is(":visible")) {
-            $(".cbs-selected-tab").addClass("blue");
-        }
-        $(".btn-area").on("click", function () {
-            // Check which area is currently visible
-            if ($(".cbs-form-area").is(":visible")) {
-                // Hide slot area and show review area  
-                $(".cbs-form-area").hide();
-                $(".confirm-area").hide();
-                $(".review-area").show();
-                $(".cbs-selected-tab").removeClass("blue");
-                $(".cbs-review-tab").addClass("blue");
-                $(".prev-area button").prop('disabled', false);
-            } else if ($(".review-area").is(":visible")) {
-                // Hide review area and show payment area
-                $(".review-area").hide();
-                $(".confirm-area").hide();
-                $(".payment-area").show();
-                $(".cbs-review-tab").removeClass("blue");
-                $(".cbs-payment-tab").addClass("blue");
-                $(".prev-area button").prop('disabled', false);
-            } else if ($(".payment-area").is(":visible")) {
-                // Hide review area and show payment area
-                $(".review-area").hide();
-                $(".payment-area").hide();
-                $(".confirm-area").show();
-                $(".prev-area button").prop('disabled', false);
-                $(".cbs-payment-tab").removeClass("blue");
-                $(".cbs-confirm-tab").addClass("blue");
-            }
-        });
+        // if ($(".cbs-form-area").is(":visible")) {
+        //     $(".cbs-selected-tab").addClass("blue");
+        // }
+        // $(".btn-area").on("click", function () {
+        //     // Check which area is currently visible
+        //     if ($(".cbs-form-area").is(":visible")) {
+        //         // Hide slot area and show review area  
+        //         $(".cbs-form-area").hide();
+        //         $(".confirm-area").hide();
+        //         $(".review-area").show();
+        //         $(".cbs-selected-tab").removeClass("blue");
+        //         $(".cbs-review-tab").addClass("blue");
+        //         $(".prev-area button").prop('disabled', false);
+        //     } else if ($(".review-area").is(":visible")) {
+        //         // Hide review area and show payment area
+        //         $(".review-area").hide();
+        //         $(".confirm-area").hide();
+        //         $(".payment-area").show();
+        //         $(".cbs-review-tab").removeClass("blue");
+        //         $(".cbs-payment-tab").addClass("blue");
+        //         $(".prev-area button").prop('disabled', false);
+        //     } else if ($(".payment-area").is(":visible")) {
+        //         // Hide review area and show payment area
+        //         $(".review-area").hide();
+        //         $(".payment-area").hide();
+        //         $(".confirm-area").show();
+        //         $(".prev-area button").prop('disabled', false);
+        //         $(".cbs-payment-tab").removeClass("blue");
+        //         $(".cbs-confirm-tab").addClass("blue");
+        //     }
+        // });
 
-        $(".prev-area").on("click", function () {
-            // Check which area is currently visible
-            if ($(".review-area").is(":visible")) {
-                // Hide slot area and show review area
-                $(".cbs-form-area").show();
-                $(".payment-area").hide();
-                $(".review-area").hide();
-                $(".confirm-area").hide();
-                $(".cbs-selected-tab").addClass("blue");
-                $(".cbs-review-tab").removeClass("blue");
+        // $(".prev-area").on("click", function () {
+        //     // Check which area is currently visible
+        //     if ($(".review-area").is(":visible")) {
+        //         // Hide slot area and show review area
+        //         $(".cbs-form-area").show();
+        //         $(".payment-area").hide();
+        //         $(".review-area").hide();
+        //         $(".confirm-area").hide();
+        //         $(".cbs-selected-tab").addClass("blue");
+        //         $(".cbs-review-tab").removeClass("blue");
 
-            } else if ($(".payment-area").is(":visible")) {
-                // Hide review area and show payment area
-                $(".review-area").show();
-                $(".cbs-form-area").hide();
-                $(".payment-area").hide();
-                $(".confirm-area").hide();
-                $(".cbs-review-tab").addClass("blue");
-                $(".cbs-payment-tab").removeClass("blue");
+        //     } else if ($(".payment-area").is(":visible")) {
+        //         // Hide review area and show payment area
+        //         $(".review-area").show();
+        //         $(".cbs-form-area").hide();
+        //         $(".payment-area").hide();
+        //         $(".confirm-area").hide();
+        //         $(".cbs-review-tab").addClass("blue");
+        //         $(".cbs-payment-tab").removeClass("blue");
 
-            } else if ($(".confirm-area").is(":visible")) {
-                // Hide review area and show payment area
-                $(".payment-area").show();
-                $(".review-area").hide();
-                $(".cbs-form-area").hide();
-                $(".confirm-area").hide();
-                $(".cbs-payment-tab").addClass("blue");
-                $(".cbs-confirm-tab").removeClass("blue");
+        //     } else if ($(".confirm-area").is(":visible")) {
+        //         // Hide review area and show payment area
+        //         $(".payment-area").show();
+        //         $(".review-area").hide();
+        //         $(".cbs-form-area").hide();
+        //         $(".confirm-area").hide();
+        //         $(".cbs-payment-tab").addClass("blue");
+        //         $(".cbs-confirm-tab").removeClass("blue");
 
-            }  else {
-                $(".prev-area button").prop('disabled', true);
-            }
-        });
+        //     }  else {
+        //         $(".prev-area button").prop('disabled', true);
+        //     }
+        // });
     });
 });
 
--- template/tab-menu.php
+++ template/tab-menu.php
@@ -1,6 +1,50 @@
 <div class="cbs-tab-section">
-    <div class="cbs-tab cbs-selected-tab"><?php esc_html_e( 'Select Slots', 'cbs' ); ?></div> >
-    <div class="cbs-tab cbs-review-tab"><?php esc_html_e( 'Review', 'cbs' ); ?></div> >
-    <div class="cbs-tab cbs-payment-tab"><?php esc_html_e( 'Payment', 'cbs' ); ?></div> >
-    <div class="cbs-tab cbs-confirm-tab"><?php esc_html_e( 'Confirmation', 'cbs' ); ?></div>
+    <?php 
+        if(is_page( 'booking' )){
+            ?>
+                <div class="cbs-tab cbs-selected-tab"><?php esc_html_e( 'Select Slots', 'cbs' ); ?></div> 
+            <?php
+        }else{
+            ?>
+                <div class="cbs-tab"><?php esc_html_e( 'Select Slots', 'cbs' ); ?></div> 
+            <?php
+        }
+    ?>
+    >
+    <?php 
+        if(is_page( 'review' )){
+            ?>
+                <div class="cbs-tab cbs-review-tab cbs-selected-tab"><?php esc_html_e( 'Review', 'cbs' ); ?></div>
+            <?php
+        }else{
+            ?>
+                <div class="cbs-tab"><?php esc_html_e( 'Review', 'cbs' ); ?></div>
+            <?php
+        }
+    ?>
+     >
+     <?php 
+        if(is_page( 'payment' )){
+            ?>
+                <div class="cbs-tab cbs-selected-tab"><?php esc_html_e( 'Payment', 'cbs' ); ?></div>
+            <?php
+        }else{
+            ?>
+                <div class="cbs-tab"><?php esc_html_e( 'Payment', 'cbs' ); ?></div>
+            <?php
+        }
+    ?>
+     >
+     <?php 
+        if(is_page( 'confirmation' )){
+            ?>
+                <div class="cbs-tab cbs-selected-tab"><?php esc_html_e( 'Confirmation', 'cbs' ); ?></div>
+            <?php
+        }else{
+            ?>
+                <div class="cbs-tab"><?php esc_html_e( 'Confirmation', 'cbs' ); ?></div>
+            <?php
+        }
+    ?>
+    
 </div>
