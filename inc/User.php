c666465888df1158ec4ed14b85d3b79e6a94bb49
Habibur Rahman <habibblog97@gmail.com>
2023-11-25 18:51:43 +0600

    custom user registration done

---
+++ inc/User.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace CBS\Src;
+
+class User{
+
+    /**
+     * Class constructor
+     */
+    function __construct() {
+        register_activation_hook( CBS, 'cbs_create_custom_user' );
+    }
+
+    /**
+     * Creatign custom user
+     */
+    function cbs_create_custom_user() {
+
+        $cbs_usr_roles = [
+            [
+                'role'          => 'basic_member',
+                'display_name'  => __( 'Basic Member', 'cbs' ),
+                'capabilities'  => [
+                    'read' => true,
+                ],
+            ],
+            [
+                'role'          => 'pro_member',
+                'display_name'  => __( 'Pro Member', 'cbs' ),
+                'capabilities'  => [
+                    'read' => true,
+                ],
+            ],
+            [
+                'role'          => 'premium_member',
+                'display_name'  => __( 'Premium Member', 'cbs' ),
+                'capabilities'  => [
+                    'read' => true,
+                ],
+            ],
+        ];
+
+        //register custom user role
+        foreach( $cbs_usr_roles as $role_data ) {
+
+            if ( ! get_role( $role_data['role'] ) ) {
+                add_role(
+                    $role_data['role'],
+                    $role_data['display_name'],
+                    $role_data['capabilities']
+                );
+            }
+        }
+
+    }
+
+}
